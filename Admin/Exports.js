38b1bf237c1e4ed06ca6d24d46fc1ac02e462e93
Saeed Nemati <sn.1361@gmail.com>
2022-05-15 10:29:19 +0430

    committed by script

---
+++ Admin/Exports.js
@@ -0,0 +1,35 @@
+import { Forms } from './Form/List'
+import { SavedForms } from './SsavedForm/List'
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+
+const FormsRoutes = [
+    {
+        "path": "/forms",
+        "component": Forms
+    },
+    {
+        "path": "/savedForms",
+        "component": SavedForms
+    }
+]
+
+const FormsMenu = [
+    {
+        "title": "Forms",
+        "icon": NoteAltIcon,
+        "children": [
+            {
+                "title": "Forms",
+                "url": "/forms"
+            },
+            {
+                "title": "Saved Forms",
+                "url": "/savedForm"
+            }
+        ]
+    }
+]
+
+export { FormsRoutes }
+export { Forms }
+export { FormsMenu }
---
+++ Admin/Form/List.js
@@ -0,0 +1,20 @@
+import { List } from '@List'
+
+const headers = <>
+    <th>Key</th>
+</>
+
+const row = (item) => <>
+    <td>{item.key}</td>
+</>
+
+const Forms = () => {
+    return <List
+        title="Forms"
+        entityType='form'
+        headers={headers}
+        row={row}
+    />
+}
+
+export default Forms;
