65d33273458cb655f407d767c6c6f3b9e2dbc5d4
Saeed Nemati <sn.1361@gmail.com>
2022-05-16 09:16:23 +0430

    committed by script

--- Admin/Exports.js
+++ Admin/Exports.js
@@ -24,7 +24,7 @@ const FormsMenu = [
             },
             {
                 "title": "Saved Forms",
-                "url": "/savedForm"
+                "url": "/savedForms"
             }
         ]
     }
